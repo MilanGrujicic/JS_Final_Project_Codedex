5fcef0fb833f60cf28e488417d3ad2f606cd4df9
Milan <milangru@gmail.com>
2024-01-28 23:25:12 +0100

    Updated js file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function fetchWeather() {
     let searchInput = document.getElementById("search").value;
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
-    const apiKey = "35e17c296b0bd189fb968b8a5a8a518d";
+    const apiKey = "";
     
     if (searchInpu == "") {
         weatherDataSection.innerHTML = `
